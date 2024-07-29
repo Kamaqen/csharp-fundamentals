bae7c83d630086405aba1d2e58df624be7674be8
Jorge Martín Capcha <jmcapcha@gmail.com>
2024-07-29 01:40:43 -0500

    ADD: greed is good kata solution

---
+++ HelloWorldConsole/katas/greed-is-good.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+
+namespace HelloWorldConsole.Katas
+{
+    public static class GreedIsGood
+    {
+        public static int Score(int[] dice)
+        {
+            var counts = new Dictionary<int, int>();
+            foreach (var die in dice)
+            {
+                if (counts.ContainsKey(die))
+                {
+                    counts[die]++;
+                }
+                else
+                {
+                    counts[die] = 1;
+                }
+            }
+
+            int total = 0;
+
+            foreach (var kvp in counts)
+            {
+                int value = kvp.Key;
+                int count = kvp.Value;
+
+                if (value == 1)
+                {
+                    total += (count >= 3 ? 1000 : 0) + (count % 3) * 100;
+                }
+                else if (value == 5)
+                {
+                    total += (count >= 3 ? 500 : 0) + (count % 3) * 50;
+                }
+                else if (count >= 3)
+                {
+                    total += value * 100;
+                }
+            }
+
+            return total;
+        }
+    }
+}
