08492c1a4e1599adb40d6508d51598de7d2cf974
Jorge Martín Capcha <jmcapcha@gmail.com>
2024-07-30 17:19:42 -0500

    ADD: grasshopper gradebook kata solution

---
+++ HelloWorldConsole/katas/Grasshopper-gradebook.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace HelloWorldConsole.Katas
+{
+    public class Grasshopper
+    {
+        public static char GetGrade(int s1, int s2, int s3)
+        {
+            var average = (s1 + s2 + s3) / 3;
+
+            if (average >= 90)
+            {
+                return 'A';
+            }
+            else if (average >= 80)
+            {
+                return 'B';
+            }
+            else if (average >= 70)
+            {
+                return 'C';
+            }
+            else if (average >= 60)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
+        }
+    }
+}
