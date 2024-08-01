d97417bd550aac006da0390834e7d45e6f1c6d14
Jorge Martín Capcha <jmcapcha@gmail.com>
2024-08-01 02:23:52 -0500

    ADD: solution to highest scoring word kata

---
+++ HelloWorldConsole/katas/highest-scoring-word.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HelloWorldConsole.Katas
+{
+    public class HighestScoringWord
+    {
+        public static string High(string s)
+        {
+            var words = s.Split(' ');
+            var scores = new List<int>();
+            foreach (var word in words)
+            {
+                var sum = word.Where(c => char.IsLetter(c)).Select(c => (c - 'a' + 1)).Sum();
+                scores.Add(sum);
+            }
+            var highestScore = new List<int>(scores);
+            highestScore.Sort();
+            var highestIndex = scores.FindIndex(score => score == highestScore.Last());
+            return words[highestIndex];
+        }
+    }
+}
