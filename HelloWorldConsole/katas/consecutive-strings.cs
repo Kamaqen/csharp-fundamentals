46df7b972041c1df35071bb2094721a590c42098
Jorge Martín Capcha <jmcapcha@gmail.com>
2024-08-06 03:21:48 -0500

    ADD: solution for consecutive strings kata

---
+++ HelloWorldConsole/katas/consecutive-strings.cs
@@ -0,0 +1,34 @@
+namespace HelloWorldConsole.Katas
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class LongestConsecutives
+    {
+        public static string LongestConsec(string[] strarr, int k)
+        {
+            // Check for invalid inputs
+            if (strarr == null || strarr.Length == 0 || k <= 0 || k > strarr.Length)
+            {
+                return "";
+            }
+
+            string longest = "";
+
+            // Loop from index 0 to (strarr.Length - k)
+            for (int i = 0; i <= strarr.Length - k; i++)
+            {
+                // Concatenate k consecutive strings starting from index i
+                string concatStr = string.Concat(strarr.Skip(i).Take(k));
+                // Update longest if the current concatenated string is longer
+                if (concatStr.Length > longest.Length)
+                {
+                    longest = concatStr;
+                }
+            }
+
+            return longest;
+        }
+    }
+}
