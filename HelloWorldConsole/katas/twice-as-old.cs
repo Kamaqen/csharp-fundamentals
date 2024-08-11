167bec44ed3ee1601b53ce6c999ebd2927fa7c76
Jorge Martín Capcha <jmcapcha@gmail.com>
2024-08-11 14:10:38 -0500

    ADD: twice as old kata solution

---
+++ HelloWorldConsole/katas/twice-as-old.cs
@@ -0,0 +1,12 @@
+namespace HelloWorldConsole.Katas
+{
+    using System;
+
+    public class TwiceAsOldSolution
+    {
+        public static int TwiceAsOld(int dadYears, int sonYears)
+        {
+            return Math.Abs(dadYears - sonYears * 2);
+        }
+    }
+}
