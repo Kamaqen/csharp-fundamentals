bc6fb769c1296334fb5909b793bc4911eb18e392
Jorge Martín Capcha <jmcapcha@gmail.com>
2024-08-12 14:42:36 -0500

    ADD: keep up the hoop kata solution

---
+++ HelloWorldConsole/katas/keep-up-the-hoop.cs
@@ -0,0 +1,12 @@
+namespace HelloWorldConsole.Katas
+{
+    public class KeepUp
+    {
+        public static string HoopCount(int n)
+        {
+            return n < 10 ? "Keep at it until you get it" : "Great, now move on to tricks";
+        }
+    }
+}
+
+// https://www.codewars.com/kata/55cb632c1a5d7b3ad0000145/train/csharp
