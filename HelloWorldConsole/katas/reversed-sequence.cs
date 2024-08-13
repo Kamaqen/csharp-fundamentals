ed45b956add87d3096b4b638afc33a31aef814e6
Jorge Martín Capcha <jmcapcha@gmail.com>
2024-08-13 01:21:04 -0500

    ADD: reversed sequence kata solution

---
+++ HelloWorldConsole/katas/reversed-sequence.cs
@@ -0,0 +1,18 @@
+namespace HelloWorldConsole.Katas
+{
+    using System;
+    using System.Collections.Generic;
+
+    public static class ReversedSeq
+    {
+        public static int[] ReverseSeq(int n)
+        {
+            int[] integers = new Int32[n];
+            for (int i = n; i > 0; i = i - 1)
+            {
+                integers.SetValue(i, n - i);
+            }
+            return integers;
+        }
+    }
+}
