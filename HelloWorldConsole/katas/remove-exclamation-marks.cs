fde2d22f21d82015ddbbe01e142445144fb1d82c
Jorge Martín Capcha <jmcapcha@gmail.com>
2024-08-13 00:00:07 -0500

    ADD: remove exclamation marks kata solution

---
+++ HelloWorldConsole/katas/remove-exclamation-marks.cs
@@ -0,0 +1,20 @@
+namespace HelloWorldConsole.Katas
+{
+    using System;
+
+    public class RemoveExclamation
+    {
+        public static string RemoveExclamationMarks(string s)
+        {
+            var newString = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i].ToString() != "!")
+                {
+                    newString += s[i];
+                }
+            }
+            return newString;
+        }
+    }
+}
