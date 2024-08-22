889a2f370a0562b7136955be1e5fd72d1a299710
Jorge Martín Capcha <jmcapcha@gmail.com>
2024-08-21 23:58:56 -0500

    ADD: count characters in your string kata solution

---
+++ HelloWorldConsole/katas/count-chars.cs
@@ -0,0 +1,28 @@
+namespace HelloWorldConsole.Katas
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class CountChars
+    {
+        public static Dictionary<char, int> Count(string str)
+        {
+            var result = new Dictionary<char, int>();
+            if (str.Length > 0)
+            {
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (result.ContainsKey(str[i]))
+                    {
+                        result[str[i]]++; // Incrementa el valor si la clave ya existe
+                    }
+                    else
+                    {
+                        result.Add(str[i], 1); // Agrega la clave con valor 1 si no existe
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
