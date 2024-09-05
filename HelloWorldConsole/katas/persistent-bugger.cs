b02c358deda919cde259fd56dff2d2c5939b7f6b
Jorge Martín Capcha <jmcapcha@gmail.com>
2024-09-04 23:34:39 -0500

    ADD: solution for persistent bugger kata

---
+++ HelloWorldConsole/katas/persistent-bugger.cs
@@ -0,0 +1,26 @@
+namespace HelloWorldConsole.Katas
+{
+    using System;
+
+    public class Persist
+    {
+        public static int Persistence(long n)
+        {
+            int persistence = 0;
+            string ene = n.ToString();
+            while (ene.Length > 1)
+            {
+                int producto = 1;
+                for (int i = 0; i < ene.Length; i++)
+                {
+                    int cifraParsed = (int)Char.GetNumericValue(ene[i]);
+                    producto = producto * cifraParsed;
+                }
+                ene = producto.ToString();
+                persistence++;
+            }
+
+            return persistence;
+        }
+    }
+}
